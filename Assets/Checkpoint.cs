8aecf0187501307ce2c1b9aa9dbe6cdbe78af721
Winson <winson.au@ynov.com>
2019-06-20 11:27:20 +0200

    projet unity

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Checkpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] Target;
+    int i = 0;
+
+ 
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(Target[0].GetComponent<Transform>().position);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<NavMeshAgent>().remainingDistance < 3)
+        {
+            i++;
+            GetComponent<NavMeshAgent>().SetDestination(Target[i].GetComponent<Transform>().position);
+           
+        }
+
+    }
+            
+
+}
---
+++ Assets/deplacement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deplacement : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+            Rigidbody r = GetComponent<Rigidbody>();
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                GetComponent<Transform>().Rotate(new Vector3(0, 1, 0));
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                GetComponent<Transform>().Rotate(new Vector3(0, -1, 0));
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                r.AddForce(-GetComponent<Transform>().right * Speed);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                r.AddForce(GetComponent<Transform>().right * Speed);
+            }
+            if (Input.GetKeyDown("space"))
+            {
+                r.AddForce(new Vector3(0, 500, 0));
+            }
+
+        
+    }
+}
